0d28af050dc88b83b6ada64805e69329ab4aa6c2
kkumtree <52643858+kkumtree@users.noreply.github.com>
2023-08-15 13:28:27 +0000

    add: experimental webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
+};
